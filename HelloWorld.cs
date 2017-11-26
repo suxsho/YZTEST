db126814b9ab9ccecd676ef3a19daee64f748686
CGStudio <suxsho@qq.com>
2017-11-26 17:11:05 +0800

    2017-11-26
    更新第一次代码

---
+++ HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        print("Hello World");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
